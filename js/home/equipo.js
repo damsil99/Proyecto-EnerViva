be463d39c8547eb69d79dd65b5f3ae1d091dc6f9
Daniel Mosquera <daniel09415@gmail.com>
2025-05-28 17:01:29 -0500

    En el index.html se reduce el tamaño de las páginas slider, además se añaden los submenús de las energías y los equipos, asimismo se hace la opacidad del navbar a scrollear en la página, también se corrió el direccionamiento de Energías Renovables, para que se fije en el home de manera visible y sin cortes, en los css se añadieron las configuraciones para darle el stilo a la página

--- js/home/equipo.js
+++ js/home/equipo.js
@@ -6,4 +6,32 @@ document.addEventListener('DOMContentLoaded', function () {
     window.closeModal = function(modalId) {
         document.getElementById(modalId).style.display = "none";
     };
+
+    window.addEventListener('scroll', function () {
+        const navbar = document.querySelector('.navbar');
+        if (window.scrollY > 10) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+    window.addEventListener('scroll', function () {
+        const navbar = document.querySelector('.desplegable .submenu');
+        if (window.scrollY > 10) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
+    window.addEventListener('scroll', function () {
+        const navbar = document.querySelector('.desplegable-team .submenu-team');
+        if (window.scrollY > 10) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
 });
