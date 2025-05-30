20e9847d7ffd70b23e6cfe8fbb346bd4956b23bd
Daniel Mosquera <daniel09415@gmail.com>
2025-05-30 03:12:57 -0500

    Se corrige el navbar, se pone general para todas la páginas diseñadas por Daniel. También se corrige los desplegables

--- js/home/equipo.js
+++ js/home/equipo.js
@@ -16,13 +16,4 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    window.addEventListener('scroll', function () {
-        const navbar = document.querySelector('.navbar .submenu');
-        if (window.scrollY > 10) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
-
 });
