3b89276700e219f448867355cc7f54cd84547011
Daniel Mosquera <daniel09415@gmail.com>
2025-05-29 23:14:33 -0500

    Se eliminan las redundancias en el código, falta corregir el navbar

--- js/home/equipo.js
+++ js/home/equipo.js
@@ -17,16 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     window.addEventListener('scroll', function () {
-        const navbar = document.querySelector('.desplegable .submenu');
-        if (window.scrollY > 10) {
-            navbar.classList.add('scrolled');
-        } else {
-            navbar.classList.remove('scrolled');
-        }
-    });
-
-    window.addEventListener('scroll', function () {
-        const navbar = document.querySelector('.desplegable-team .submenu-team');
+        const navbar = document.querySelector('.navbar .submenu');
         if (window.scrollY > 10) {
             navbar.classList.add('scrolled');
         } else {
